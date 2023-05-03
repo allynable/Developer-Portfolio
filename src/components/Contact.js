70bd492b0ca946bd1126a5e6c5ef081ba334eb6e
icable24 <114415997+allynable@users.noreply.github.com>
2023-05-03 12:30:03 +0800

    added emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,97 +1,69 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import { Container, Row, Col } from "react-bootstrap";
-import TrackVisibility from 'react-on-screen';
 import ContactImage from "../assets/img/Contact.png";
 import "../assets/styles/contact.css";
 
 const Contact = () => {
-  const formInitialDetails = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    message: "",
-  };
+  const form = useRef();
 
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState('Send');
-  const [status, setStatus] = useState({});
-
-  const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
-  }
-
-  const handleSubmit = async (e) => {
+  const [done, setDone] = useState(false);
+  const sendEmail = (e) => {
     e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("http://localhost:3000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully'});
-    } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
-    }
+
+    emailjs.sendForm('service_o440x7p', 'template_zbkho7q', form.current, 'ZZZLrL-o-C4Tbbmz1')
+      .then((result) => {
+          console.log(result.text);
+          setDone(true);
+          setTimeout(() => {
+            setDone(false);
+          }, 5000);
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
   };
+
   return (
     <section className="contact" id="contact">
       <Container>
         <Row className="align-items-center">
           <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={ContactImage} alt="Contact Us"/>
-              }
-            </TrackVisibility>
+            <img src={ContactImage} alt="Contact Us" />
           </Col>
           <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Get In Touch</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                      </Col>
-                    }
-                  </Row>
-                </form>
-              </div>}
-            </TrackVisibility>
+            <div>
+              <h2>Get In Touch</h2>
+              <form ref={form} onSubmit={sendEmail}>
+                <Row>
+                  <Col size={12} sm={12} className="px-1">
+                    <input type="text" placeholder="Name" name="name" required/>
+                  </Col>
+                  <Col size={12} sm={12} className="px-1">
+                    <input type="email" placeholder="Email Address" name="email" required/>
+                  </Col>
+                  <Col size={12} sm={12} className="px-1">
+                    <input type="tel" placeholder="Contact No." name="contact" required/>
+                  </Col>
+                  <Col size={12} sm={12} className="px-1">
+                    <textarea rows="6" placeholder="Message" name="message" required></textarea>
+                  </Col>
+                  <Col size={12} sm={12} class="text-center">
+                    <h5>{done && "Thanks for contacting me!"}</h5>
+                  </Col>
+                  <Col size={12} sm={12} className="text-center">
+                    <button type="submit" className="btn btn-send">
+                      Send
+                    </button>
+                  </Col>
+                </Row>
+              </form>
+            </div>
           </Col>
         </Row>
       </Container>
     </section>
-  )
+  );
 };
 
 export default Contact;
