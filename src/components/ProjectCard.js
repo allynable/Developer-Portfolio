3fd84238bd4e6d537d2fdc882879a97f3fd31ad8
icable24 <114415997+allynable@users.noreply.github.com>
2023-05-05 15:56:10 +0800

    updated projects section

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { Col} from 'react-bootstrap';
-import { FaGithub } from 'react-icons/fa';
+import React from "react";
+import { Col } from "react-bootstrap";
+import { FaGithub } from "react-icons/fa";
 
-export const ProjectCard = ({title, imageUrl, details, githubLink}) => {
+export const ProjectCard = ({ title, imageUrl, details, githubLink }) => {
   return (
     <Col sm={4}>
       <div className="proj-imgbx">
-        <img src={imageUrl} className="img-fluid"/>
+        <img src={imageUrl} className="img-fluid" alt="" />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{details}</span>
           <br />
-          <a href={githubLink} target="_blank" rel="noopener noreferrer">
-            <FaGithub size={28}  className="social-icons" title="Go to GitHub Repository"/>
-         </a>
+          {githubLink ? (
+            <a href={githubLink} target="_blank" rel="noopener noreferrer">
+              <FaGithub
+                size={28}
+                className="social-icons mt-5"
+                title="Go to GitHub Repository"
+              />
+            </a>
+          ) : null}
         </div>
       </div>
     </Col>
-  )
-}
+  );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,63 +1,65 @@
-import React from 'react'
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import projectImg1 from "../assets/img/silent-library-banner.png";
 import projectImg2 from "../assets/img/boutiqa-banner.png";
 import projectImg3 from "../assets/img/prcbbmis-banner.png";
 import projectImg4 from "../assets/img/tumandok-banner.png";
-import {ProjectCard} from "./ProjectCard";
+import projectImg5 from "../assets/img/online-portal.png";
+import { ProjectCard } from "./ProjectCard";
 import "../assets/styles/projects.css";
 const Projects = () => {
-    const projects = [
-        {
-            title: "Silent Library",
-            imageUrl: projectImg1,
-            details: "HTML, CSS, Javascript",
-            githubLink: "https://github.com/allynable/Silent-Library"
-        },
-        {
-            title: "Boutiqa - Ecommerce Portal",
-            imageUrl: projectImg2,
-            details: "Spring MVC, Bootstrap, MySQL",
-            githubLink: "https://github.com/allynable/Silent-Library"
-        },
-        {
-            title: "Philippine Red Cross Blood Bank Management Information System",
-            imageUrl: projectImg3,
-            details: "PHP, Bootstrap, MySQL",
-            githubLink: "https://github.com/allynable/prcbbmis"
-        }, 
-        {
-            title: "Tumandok Craft Industries Website",
-            imageUrl: projectImg4,
-            details: "PHP, Bootstrap, MySQL",
-            githubLink: "https://github.com/allynable/tci"
-        }
-    ];
+  const projects = [
+    {
+      title: "Silent Library",
+      imageUrl: projectImg1,
+      details: "HTML, CSS, Javascript",
+      githubLink: "https://github.com/allynable/Silent-Library",
+    },
+    {
+      title: "Boutiqa - Ecommerce Portal",
+      imageUrl: projectImg2,
+      details: "Spring MVC, Bootstrap, MySQL",
+      githubLink: "https://github.com/allynable/spring-boutiqa",
+    },
+    {
+        title: "Online News Portal",
+        imageUrl: projectImg5,
+        details: "Liferay"        
+    },
+    {
+      title: "Philippine Red Cross Blood Bank Management Information System",
+      imageUrl: projectImg3,
+      details: "PHP, Bootstrap, MySQL",
+        githubLink: "https://github.com/allynable/prcbbmis",
+    },
+    {
+      title: "Tumandok Craft Industries Website",
+      imageUrl: projectImg4,
+      details: "PHP, Bootstrap, MySQL",
+      githubLink: "https://github.com/allynable/tci",
+    }
+  ];
   return (
     <section className="project" id="projects">
-        <Container>
+      <Container>
+        <Row>
+          <Col size={12}>
+            <h2>Projects</h2>
+            <p>
+              Here you'll find a collection of my latest and greatest work,
+              showcasing my skills and expertise in various areas.
+            </p>
+
             <Row>
-                <Col size={12}>
-                    <h2>Projects</h2>
-                    <p>Here you'll find a collection of my latest and greatest work, showcasing my skills and expertise in various areas.</p>
-
-                    <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                    </Row>
-                </Col>
+              {projects.map((project, index) => {
+                return <ProjectCard key={index} {...project} />;
+              })}
             </Row>
-        </Container>
+          </Col>
+        </Row>
+      </Container>
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
